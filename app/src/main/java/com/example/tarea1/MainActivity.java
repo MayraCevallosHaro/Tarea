d52d3647995ae8615222eb8a2983b46066282c6f
MayraCevallosHaro <57886405+MayraCevallosHaro@users.noreply.github.com>
2020-11-24 20:25:50 -0500

    Initial commit

--- app/src/main/java/com/example/tarea1/MainActivity.java
+++ app/src/main/java/com/example/tarea1/MainActivity.java
@@ -3,27 +3,44 @@ package com.example.tarea1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
+    // Declarando los controles a utilizar
+    private EditText et1, et2, et3;
+    private RadioButton rb1, rb2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et1 = (EditText)findViewById(R.id.txtNombre);
+        et2 = (EditText)findViewById(R.id.txtNaci);
+        et3 = (EditText)findViewById(R.id.txtTelefono);
+        rb1=(RadioButton)findViewById(R.id.rbclick);
+        rb2=(RadioButton)findViewById(R.id.RBtnFemenino);
     }
     public void btEnviar(View view){ //Creamos el Intent
+        String nombre = et1.getText().toString();
+        String nacimiento = et2.getText().toString();
+        String telefono = et3.getText().toString();
+        String genero = "";
+        if(rb1.isChecked()) genero="Masculino";
+        else if(rb2.isChecked()) genero="Femenino";
+        else genero="No especificado";
+        //Creacion del intent
         Intent intent = new Intent(MainActivity.this, MainActivity3.class);
-        EditText txtNombre = (EditText)findViewById(R.id.txtNombre);
-        //Creamos la información a pasar entre actividades - Pares Key-Value
+        //Información a pasarse
         Bundle b = new Bundle();
-        b.putString("NOMBRE", txtNombre.getText().toString());
-        //Añadimos la información al intent
+        String [] valores = {nombre,genero, nacimiento,telefono};
+        //Ingresando los datos al bundle
+        b.putStringArray("Valores",valores);
+        //Asignando el bundle en el intent
         intent.putExtras(b);
-        // / Iniciamos la nueva actividad
         startActivity(intent);
         }
-
     }
--- app/src/main/java/com/example/tarea1/MainActivity3.java
+++ app/src/main/java/com/example/tarea1/MainActivity3.java
@@ -6,18 +6,32 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity3 extends AppCompatActivity {
-
+    private TextView tv1, tv2, tv3, tv4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-//Localizar los controles
-        TextView Saludo = (TextView)findViewById(R.id.Saludo);
-        //Recuperamos la información pasada en el intent
-        Bundle bundle = this.getIntent().getExtras();
-        //Construimos el mensaje a mostrar
-        Saludo.setText(bundle.getString("NOMBRE"));
+        tv1 = (TextView) findViewById(R.id.Saludo);
+        tv2 = (TextView) findViewById(R.id.Genero);
+        tv3 = (TextView) findViewById(R.id.lblNacimiento);
+        tv4 = (TextView) findViewById(R.id.telefono);
+
+//Recuperamos lo traído por el Bundle
+        Bundle b = this.getIntent().getExtras();
+        String valores[]=b.getStringArray("Valores");
+
+        //Validaciones
+        if(valores[0].equals("")) valores[0]="No especificado";
+        if(valores[2].equals("")) valores[2]="No especificado";
+        if(valores[3].equals("")) valores[3]="No especificado";
+        if(!valores[2].contains("/")&&!valores[2].contains("-")) valores[2]="Fecha no válida";
+        if(valores[2].length()>10) valores[2]="Fecha no válida";
 
 
+        //Ingresando los valores
+        tv1.setText(valores[0]);
+        tv2.setText(valores[1]);
+        tv3.setText(valores[2]);
+        tv4.setText(valores[3]);
     }
 }
